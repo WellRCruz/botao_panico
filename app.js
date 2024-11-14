f79e0d75bd7566f93f4960c8d30f1db5bbd2ca5f
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-11-13 22:30:21 -0300

    Botão Pânico

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Simulador Botão Pânico
+ * @author Wellington R. Cruz
+ */
+
+// variáveis
+const botao_panico = document.getElementById('panico_botao');
+let som = new Audio("sound/alarme.mp3")
+
+// Iniciar o som quando o botão for pressionado
+botao_panico.addEventListener('mousedown', function() {
+    som.play() // Iniciar Audio
+});
+
+// Parar o som quando o botão for solto
+botao_panico.addEventListener('mouseup', function() {
+    som.pause() // Para ou pausa o audio
+});
