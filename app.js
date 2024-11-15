5f9e3138a350e7734df801a7a50a80438eeed31b
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-11-14 21:44:27 -0300

    botao pânico

--- app.js
+++ app.js
@@ -10,9 +10,56 @@ let som = new Audio("sound/alarme.mp3")
 // Iniciar o som quando o botão for pressionado
 botao_panico.addEventListener('mousedown', function() {
     som.play() // Iniciar Audio
+    ligar()
 });
 
 // Parar o som quando o botão for solto
 botao_panico.addEventListener('mouseup', function() {
     som.pause() // Para ou pausa o audio
+    desligar()
 });
+
+//lanter (torch)....async (multi processamento) function (variavel)
+async function inicializarLanterna(){
+    //try-catch (tratamento de exceções)
+    try {
+        // Solicita acesso à câmera traseira sem exibir o vídeo
+        stream = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: "environment" }
+        })
+       
+        // Obtém o track do vídeo para controlar a lanterna
+        track = stream.getVideoTracks()[0]
+       
+        // Verifica se o dispositivo suporta o uso da lanterna
+        const capabilities = track.getCapabilities()
+        if (!capabilities.torch) {
+            console.log("Lanterna não suportada no dispositivo.")
+            return
+        }
+    } catch (error) {
+        console.error(`Erro ao inicializar a lanterna: ${error}`)
+    }
+}
+ 
+// Função para ligar a lanterna (torch)
+async function ligar() {
+    if (track) {
+        try {
+            await track.applyConstraints({ advanced: [{ torch: true }] })
+        } catch (error) {
+            console.error(`Erro ao inicializar a lanterna: ${error}`)
+        }
+    }
+ 
+}
+ // Função para desligar a lanterna sem parar o stream
+async function desligar() {
+    if (track) {
+        try {
+            await track.applyConstraints({ advanced: [{ torch: false }] })
+        } catch (error) {
+            console.error(`Erro ao inicializar a lanterna: ${error}`)
+        }
+    }
+}
